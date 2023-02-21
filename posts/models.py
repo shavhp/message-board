9e2bcdd7b7d180ad38d26764bb0c1f9415d99e4b
shavhp <sharelle.plein@outlook.com>
2023-02-21 16:32:11 +0100

    Creates home page and admin page

--- posts/models.py
+++ posts/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+
+# The model below is called Post
+# The type of content it will hold is TextField
 class Post(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
